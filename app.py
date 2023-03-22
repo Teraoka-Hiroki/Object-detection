9f1f021b0553407b45792abda5522c070fad1252
Teraoka-Hiroki <88216511+Teraoka-Hiroki@users.noreply.github.com>
2023-03-23 05:07:55 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,90 @@
+# 以下を「app.py」に書き込み
+import cv2
+import numpy as np
+import tensorflow as tf
+import streamlit as st
+import matplotlib.pyplot as plt
+from PIL import Image
+#import model
+from model import draw_boxes
+from model import detectors
+from model import draw_bounding_box_on_image
+#from model import download_and_resize_image
+from model import display_image
+
+#5. 物体検出に使用する画像データを検出用にデコードする関数
+
+def load_img(img):
+    img = np.asarray(img).astype(np.float32)
+    return img
+
+
+
+#6. 物体検出を実行し、バウンディングボックスが描画された画像を出力する関数
+def run_detector(detector,img):
+    img =img
+    img = load_img(img)
+
+    converted_img  = tf.image.convert_image_dtype(img, tf.float32)[tf.newaxis, ...]
+    result = detector(converted_img)
+
+    result = {key:value.numpy() for key,value in result.items()}
+
+    # バウンディングボックスを画像上に描画する
+    image_with_boxes = draw_boxes(
+
+        img, result["detection_boxes"],
+        result["detection_class_entities"], result["detection_scores"])
+    # バウンディングボックスが描画された画像を出力
+
+    return image_with_boxes
+
+
+
+
+
+st.set_option("deprecation.showfileUploaderEncoding", False)
+
+st.sidebar.title("物体検出アプリ")
+st.sidebar.write("「モデル：ssd+mobilenet V2（小規模で高速）」を使用")
+
+st.sidebar.write("")
+
+img_source = st.sidebar.radio("画像のソースを選択してください。",
+                              ("画像をアップロード", "カメラで撮影"))
+
+#detect = detectors()
+
+if img_source == "画像をアップロード":
+    img_file = st.sidebar.file_uploader("画像を選択してください。", type=["jpg"])
+elif img_source == "カメラで撮影":
+    device = user_input = st.text_input("input your video/camera device", "0")
+    if device.isnumeric():
+        device = int(device)
+#    cap = cv2.VideoCapture(device)
+#    img_file = cap
+
+    img_file = st.camera_input("カメラで撮影")
+
+if img_file is not None:
+    with st.spinner("推定中..."):
+        img = Image.open(img_file)
+        st.image(img, caption="対象の画像", width=480)
+        st.write("")
+
+        # 予測
+
+#7. 物体検出を実行し、検出結果を反映させた画像を出力
+        detect = detectors()
+        image = img.resize((640, 480))
+
+
+
+
+        result_img = run_detector(detect,image)
+
+        image = cv2.cvtColor(result_img, cv2.COLOR_BGR2RGB)
+        cv2.imwrite('temporary.jpg', image)
+        # 結果の表示
+        st.subheader("判定結果")
+        st.image('temporary.jpg', caption="検出の画像", width=480)
---
+++ model.py
@@ -0,0 +1,120 @@
+# 以下を「model.py」に書き込み
+
+#TensorFlow,TensorFlow Hub
+import tensorflow as tf
+import tensorflow_hub as hub
+
+# イメージの出力のためのライブラリ
+import matplotlib.pyplot as plt
+# イメージのダウンロードのためのライブラリ
+import tempfile
+from six.moves.urllib.request import urlopen
+from six import BytesIO
+# イメージの出力のためのライブラリ
+import numpy as np
+from PIL import Image
+from PIL import ImageColor
+from PIL import ImageDraw
+from PIL import ImageFont
+from PIL import ImageOps
+
+def display_image(image):
+
+    fig = plt.figure(figsize=(20, 15))
+    plt.grid(False)
+    plt.imshow(image)
+
+def download_and_resize_image(
+    img, new_width=256, new_height=256, display=False):
+#    '''イメージをダウンロードして指定のサイズにリサイズする
+#    '''
+    _, filename = tempfile.mkstemp(suffix=".jpg")
+    image_data =img
+    pil_image = Image.open(image_data)
+    pil_image = ImageOps.fit(pil_image, (new_width, new_height), Image.ANTIALIAS)
+    return pil_image  # イメージのパスを返す
+
+
+def draw_bounding_box_on_image(image,
+                               ymin, xmin, ymax, xmax,
+                               color,
+                               font,
+                               thickness=4,
+                               display_str_list=()):
+#    '''イメージ上にバウンディングボックスを描画する
+#    '''
+    draw = ImageDraw.Draw(image)
+    im_width, im_height = image.size
+    (left, right, top, bottom) = (xmin * im_width, xmax * im_width,
+                                    ymin * im_height, ymax * im_height)
+    draw.line(
+        [(left, top), (left, bottom), (right, bottom), (right, top),(left, top)],
+        width=thickness,
+        fill=color)
+
+    # バウンディングボックスの上部に表示するラベルが画像の上部を超える場合は、
+    # ラベルをバウンディングボックスの下部に表示する
+    display_str_heights = [font.getsize(ds)[1] for ds in display_str_list]
+    # ラベルに表示する文字列の上下にマージンを設定
+    total_display_str_height = (1 + 2 * 0.05) * sum(display_str_heights)
+
+    if top > total_display_str_height:
+        text_bottom = top
+    else:
+        text_bottom = top + total_display_str_height
+    # ラベルに表示する文字列のリストを逆順で出力
+    for display_str in display_str_list[::-1]:
+        text_width, text_height = font.getsize(display_str)
+        margin = np.ceil(0.05 * text_height)
+        draw.rectangle([(left, text_bottom - text_height - 2 * margin),
+                        (left + text_width, text_bottom)],
+                    fill=color)
+        draw.text((left + margin, text_bottom - text_height - margin),
+                display_str,
+                fill="black",
+                font=font)
+        text_bottom -= text_height - 2 * margin
+
+
+def draw_boxes(image, boxes, class_names, scores, max_boxes=10, min_score=0.1):
+    colors = list(ImageColor.colormap.values())
+
+    try:
+        font = ImageFont.truetype(
+            '/usr/share/fonts/truetype/liberation/LiberationSansNarrow-Regular.ttf',
+            25)
+    except IOError:
+        print("Font not found, using default font.")
+        font = ImageFont.load_default()
+
+    for i in range(min(boxes.shape[0], max_boxes)):
+        if scores[i] >= min_score:
+            ymin, xmin, ymax, xmax = tuple(boxes[i])
+            display_str = "{}: {}%".format(class_names[i].decode("ascii"),
+                                        int(100 * scores[i]))
+            color = colors[hash(class_names[i]) % len(colors)]
+            image_pil = Image.fromarray(np.uint8(image)).convert("RGB")
+            draw_bounding_box_on_image(
+                image_pil,
+                ymin,
+                xmin,
+                ymax,
+                xmax,
+                color,
+                font,
+                display_str_list=[display_str])
+            np.copyto(image, np.array(image_pil))
+    return image
+
+def detectors():
+# ssd+mobilenet V2: 小規模で高速
+# https://tfhub.dev/google/openimages_v4/ssd/mobilenet_v2/1
+
+#  FasterRCNN+InceptionResNet V2を使用
+#    module_handle = "https://tfhub.dev/google/faster_rcnn/openimages_v4/inception_resnet_v2/1" 
+    module_handle = "https://tfhub.dev/google/openimages_v4/ssd/mobilenet_v2/1"
+    detector = hub.load(module_handle).signatures['default']
+    return detector
+
+
+
